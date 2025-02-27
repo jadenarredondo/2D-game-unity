5f6f8aca45bc6b87ebe0393440cefcf1ae10d56a
jadenarredondo <jaden.arredondo01@utrgv.edu>
2025-02-27 14:33:17 -0600

    first game

---
+++ Assets/Scripts/Hello_world.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Hello_world : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+    void Update()
+    {
+        Debug.Log("Updating!");
+    }
+}
+
